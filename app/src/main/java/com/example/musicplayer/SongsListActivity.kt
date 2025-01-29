64c01dbe1ee4b37eb2f9cff5ca1a86c63d87d36a
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-01-30 00:08:35 +0530

    Activity to display all the songs in a category

--- app/src/main/java/com/example/musicplayer/SongsListActivity.kt
+++ app/src/main/java/com/example/musicplayer/SongsListActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.musicplayer.adapter.SongListAdapter
+import com.example.musicplayer.databinding.ActivitySongsListBinding
+import com.example.musicplayer.models.CategoryModels
 
 class SongsListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySongsListBinding
+    private lateinit var songListAdapter: SongListAdapter
+
+    companion object{
+        lateinit var categories: CategoryModels
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_songs_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySongsListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            binding.nameTextView.text = categories.name
+            Glide.with(binding.coverImageView)
+                .load(categories.coverUrl)
+                .apply(
+                    RequestOptions().transform(RoundedCorners(32))
+                )
+                .into(binding.coverImageView)
+
+            setupSongRecyclerView()
         }
+    }
+
+    fun setupSongRecyclerView(){
+        songListAdapter = SongListAdapter(categories.songs)
+        binding.songsListRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.songsListRecyclerView.adapter = songListAdapter
     }
 }
