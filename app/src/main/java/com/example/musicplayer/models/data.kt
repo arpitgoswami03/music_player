26ccb8d53b4a83678b6598e708bb8dcaa31110fc
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-01-31 16:23:15 +0530

    this folder now contains all the data class of this project

--- app/src/main/java/com/example/musicplayer/models/data.kt
+++ app/src/main/java/com/example/musicplayer/models/data.kt
@@ -7,3 +7,13 @@ data class CategoryModels(
 ){
     constructor(): this("","", listOf())
 }
+
+data class SongModel(
+    val id: String,
+    val title: String,
+    val singer_name: String,
+    val url: String,
+    val coverUrl: String,
+) {
+    constructor() : this("", "", "", "", "")
+}
