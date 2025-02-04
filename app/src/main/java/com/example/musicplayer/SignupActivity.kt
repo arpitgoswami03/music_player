8aaa5bd0005f45ff5602e91668c2273373b2afa4
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-04 19:19:00 +0530

    setup activity for new users

--- app/src/main/java/com/example/musicplayer/SignupActivity.kt
+++ app/src/main/java/com/example/musicplayer/SignupActivity.kt
@@ -1,20 +1,84 @@
 package com.example.musicplayer
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.example.musicplayer.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createAccountBtn.setOnClickListener {
+            val email = binding.emailEdittext.text.toString()
+            val password = binding.passwordEdittext.text.toString()
+            val confirmPassword = binding.confirmPasswordEdittext.text.toString()
+
+            if(!Pattern.matches(Patterns.EMAIL_ADDRESS.pattern(),email)){
+                binding.emailEdittext.error = "Invalid email"
+                return@setOnClickListener
+            }
+            if(password.length < 6){
+                binding.passwordEdittext.error = "Length should be 6 char"
+                return@setOnClickListener
+            }
+            if(password != confirmPassword){
+                binding.confirmPasswordEdittext.error = "Password not matched"
+                return@setOnClickListener
+            }
+            createAccountWithFirebase(email,password)
+        }
+        binding.gotoLoginBtn.setOnClickListener {
+            finish()
         }
     }
+
+    private fun createAccountWithFirebase(email : String, password: String){
+        setInProgress(true)
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                setInProgress(false)
+                Toast.makeText(applicationContext,"User created successfully",Toast.LENGTH_SHORT).show()
+                finish()
+            }.addOnFailureListener {
+                setInProgress(false)
+                Toast.makeText(applicationContext,"Create account failed",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+    private fun setInProgress(inProgress : Boolean){
+        if(inProgress){
+            binding.createAccountBtn.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
+        }else{
+            binding.createAccountBtn.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
