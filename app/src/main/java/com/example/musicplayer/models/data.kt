a0197d55cbad3b953a0d1f9dcc8e4af92bdc77b3
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-04 19:19:40 +0530

    no major change

--- app/src/main/java/com/example/musicplayer/models/data.kt
+++ app/src/main/java/com/example/musicplayer/models/data.kt
@@ -3,7 +3,7 @@ package com.example.musicplayer.models
 data class CategoryModels(
     val name: String,
     val coverUrl: String,
-    val songs: List<String>
+    var songs: List<String>
 ){
     constructor(): this("","", listOf())
 }
